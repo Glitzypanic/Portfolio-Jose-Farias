4e447ba71085917b6aa1c070e1776bc3d384f329
Glitzypanic <jose.fariasdv@gmail.com>
2024-04-20 17:57:06 -0700

    Update dependencies and configuration files for Astro and TailwindCSS

---
+++ tailwind.config.mjs
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
+    },
+    colors: {
+		'primary': '#F9F4F0',
+		'secondary': '#25452f',
+    },
+    fontFamily: {
+      sans: ["Graphik", "sans-serif"],
+      serif: ["Merriweather", "serif"],
+    },
+    extend: {
+      spacing: {
+        128: "32rem",
+        144: "36rem",
+      },
+      borderRadius: {
+        "4xl": "2rem",
+      },
+    },
+    extend: {},
+  },
+  plugins: [],
+};
